fa396229f4c9bad0aff8b96de6369950222d4147
Henrik Boulund <>
2023-03-21 09:39:03 +0100

    Init

---
+++ src/be/PC.java
@@ -0,0 +1,33 @@
+package be;
+
+public class PC extends Computer
+{
+    private String ram;
+    private String hdd;
+    private String cpu;
+
+    public PC(String ram, String hdd, String cpu)
+    {
+        this.ram=ram;
+        this.hdd=hdd;
+        this.cpu=cpu;
+    }
+
+    @Override
+    public String getRAM()
+    {
+        return null;
+    }
+
+    @Override
+    public String getHDD()
+    {
+        return null;
+    }
+
+    @Override
+    public String getCPU()
+    {
+        return null;
+    }
+}
